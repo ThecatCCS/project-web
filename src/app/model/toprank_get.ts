324c8fc2a89de6099f0fbfb32a0a4f5d55a04839
sasithon33778655 <sasithonboonyen0@gmail.com>
2024-03-13 21:00:08 +0700

    send

---
+++ src/app/model/toprank_get.ts
@@ -0,0 +1,8 @@
+export interface ToprankGetResponse {
+    pictrue_id:      number;
+    pictrue_url:     string;
+    u_id:            number;
+    rank_difference: number;
+    pictrue_p:       number;
+
+}
--- src/app/top10/top10.component.ts
+++ src/app/top10/top10.component.ts
@@ -2,22 +2,51 @@ import { Component } from '@angular/core';
 import { Navtop10Component } from "../nav/navtop10/navtop10.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Constants } from '../config/constants';
+import { CommonModule } from '@angular/common';
+import { ToprankGetResponse } from '../model/toprank_get';
+import { UserService } from '../services/api/user.service';
+import { UserGetResponse } from '../model/user_get';
 @Component({
     selector: 'app-top10',
     standalone: true,
     templateUrl: './top10.component.html',
     styleUrl: './top10.component.scss',
-    imports: [ Navtop10Component,MatToolbarModule,MatIconModule]
+    imports: [ Navtop10Component,MatToolbarModule,MatIconModule,CommonModule]
 })
 export class Top10Component{
     i: number = 0;
+    toprank : ToprankGetResponse [] = [];
 
+constructor(private http: HttpClient, private constants: Constants,private userService: UserService){}
     ngOnInit(): void {
         const counterIcons = document.querySelectorAll('.counter-icon');
         counterIcons.forEach((icon: Element) => {
             this.i++; 
             (icon as HTMLElement).textContent = String(this.i); 
         });
+        this.getpicture();
     }
+    async getpicture() {
+        try {
+          const url = `${this.constants.API_ENDPOINT}/pictrue/alls`;
+          const data = await lastValueFrom(this.http.get<ToprankGetResponse[]>(url));
+          this.toprank = data;
+          console.log("รูปจะออก", this.toprank);
+          this.updateCounterIcons();
+        } catch (error) {
+          console.error('Error fetching pictures:', error);
+        }
+      }
     
-}
+      updateCounterIcons() {
+        this.toprank.forEach((item, index) => {
+          (item as any).counter = index + 1;
+        });
+      }
+      getUserName() {
+       
+      }
+    }
