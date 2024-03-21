c7f9679cd5fed634c89600539590ba0ef69d7bd5
CAT <bigstirnabon@gmail.com>
2024-03-21 21:29:16 +0700

    e

--- src/app/profile/editprofile/editprofile.component.ts
+++ src/app/profile/editprofile/editprofile.component.ts
@@ -7,6 +7,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { HttpClient } from '@angular/common/http';
+import { Constants } from '../../config/constants';
 @Component({
   selector: 'app-update-profile-dialog',
   standalone: true,
@@ -33,7 +35,9 @@ export class UpdateProfileDialogComponent {
     { value: 1, name: 'female' },
     { value: 2, name: 'Male' }
   ];
+  constructor(private http: HttpClient,private constants: Constants){
 
+  }
   edit() {
     
   }
@@ -51,14 +55,39 @@ export class UpdateProfileDialogComponent {
   }
   user: any = {}; // Define user property here
 
-  constructor(
-    public dialogRef: MatDialogRef<UpdateProfileDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
+  
+  onFileSelected3(event: any , u_id : number) {
+    const file = event.target.files[0];
+    const url = this.constants.API_ENDPOINT + `/upload/userpictrue/${u_id}`;
+    const formData = new FormData();
+    formData.append('filename', file, file.name);
+    console.log(formData, "test");
+  
+    this.http.put(url, formData).subscribe(
+      (response) => {
+        console.log('File uploaded successfully:', response);
+        this.addupdate(u_id); // เรียกใช้ addupdate() พร้อมกับการส่ง u_id ไป
+      },
+      (error) => {
+        console.error('Error uploading file:', error);
+      }
+    );
   }
+  
+  addupdate(u_id: number) {
+    const body = {
+      user_name: this.name,
+      user_gender: this.gender,
+      user_age: this.age,
+      user_preference: this.pefer,
+    };
+    const url = this.constants.API_ENDPOINT + `/upload/userprofile/${u_id}`;
+    this.http.put(url, body).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+
 }
 interface Gender {
   value: number;
