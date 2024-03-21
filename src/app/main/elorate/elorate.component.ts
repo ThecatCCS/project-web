5867324d2b936f939ee3b19e85f61710f1d9f5f3
sasithon33778655 <160719842+ThecatCCS@users.noreply.github.com>
2024-03-22 00:29:38 +0700

    ererer

---
+++ src/app/main/elorate/elorate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elorate',
+  standalone: true,
+  imports: [],
+  templateUrl: './elorate.component.html',
+  styleUrl: './elorate.component.scss'
+})
+export class ElorateComponent {
+
+}
