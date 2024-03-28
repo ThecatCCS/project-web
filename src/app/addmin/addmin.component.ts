b52e0b20bd8e5a6b58e6b8a0cb30613ee0e8db89
sasithon33778655 <160719842+ThecatCCS@users.noreply.github.com>
2024-03-29 00:18:46 +0700

    ft

--- src/app/addmin/addmin.component.ts
+++ src/app/addmin/addmin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
@@ -9,6 +9,7 @@ import { lastValueFrom } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { Location } from '@angular/common';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-addmin',
   standalone: true,
@@ -19,9 +20,12 @@ import { Location } from '@angular/common';
 export class AddminComponent implements OnInit {
  user : UserGetResponse [] = [];
  timeIn: number | undefined;
-  constructor(private rout : Router , private constants:Constants , private http : HttpClient,   private location : Location){}
+  constructor(private rout : Router , private constants:Constants , private http : HttpClient,    private dialog: MatDialog,   private location : Location){}
   ngOnInit(): void {
+
     this.getUser();
+ 
+    
 }
 logout() {
   sessionStorage.removeItem('currentUser');
@@ -32,7 +36,7 @@ logout() {
     const url = this.constants.API_ENDPOINT + `/userPro`;
     let data = await lastValueFrom(this.http.get(url));
     this.user = data as UserGetResponse[];
-    
+ 
   }
 
   Online(id: number) {
@@ -46,16 +50,58 @@ logout() {
     this.http.put(url, null).subscribe(
       (response) => {
  
+        console.log("เวลาถูกเซ็ทแล้ว");
         
       },
       (error) => {
         console.error("An error occurred:", error);
       }
-    );  
+    );
+    const downloadDialogRef = this.dialog.open(DownloadDialogComponent, {
+      data: {},
+    });
+    setTimeout(() => {
+      console.log('เปิดไดอล็อกดาวน์โหลด');
+
+      downloadDialogRef.close();
+    }, 1000); 
+
   }
-  
- 
-
-
-  
 }
+ 
+@Component({
+  selector: 'download-dialog',
+  template: `
+   <div class="loader"></div>
+   <style>
+/* HTML: <div class="loader"></div> */
+/* HTML: <div class="loader"></div> */
+.loader {
+  width: 80px;
+  height: 70px;
+  border: 5px solid #000;
+  padding: 0 8px;
+  box-sizing: border-box;
+  background:
+    linear-gradient(#fff 0 0) 0    0/8px 20px,
+    linear-gradient(#fff 0 0) 100% 0/8px 20px,
+    radial-gradient(farthest-side,#fff 90%,#0000) 0 5px/8px 8px content-box,
+    #000;
+  background-repeat: no-repeat; 
+  animation: l3 2s infinite linear;
+}
+@keyframes l3{
+  25% {background-position: 0 0   ,100% 100%,100% calc(100% - 5px)}
+  50% {background-position: 0 100%,100% 100%,0    calc(100% - 5px)}
+  75% {background-position: 0 100%,100%    0,100% 5px}
+}
+   </style>
+  `,
+})
+export class DownloadDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  download() {
+
+  }
+}
+
